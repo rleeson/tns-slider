b89e0bfa8ab8c876fe6608e3699e530f53c6f644
rleeson <sysadmin@ryanleeson.com>
2015-07-05 15:47:46 -0400

    Add infinite scroll capability
    - Scrolls partially if there number of total slides is not evenly divisible by the visible slides

--- src/js/core.js
+++ src/js/core.js
@@ -6,7 +6,7 @@ var console = console || { log: function( e ) { return; } };
 var debug	= debug || { enabled: true, log: function( e ) { if ( this.enabled ) { console.log( e ) } } }; 
 var tns 	= tns || {};
 
-tns.slider_infinite = function( $ ) {
+tns.slider_infinite = function( $, _ ) {
 	var base_element	= null,
 		base_selector	= '',
 		element_count	= 0,
@@ -19,6 +19,9 @@ tns.slider_infinite = function( $ ) {
 		slider_body		= null,
 		slides_visible	= 0;
 	
+	/**
+	 * Initial base properties of the slider
+	 */
 	function base_properties( base ) {
 		// Check for a valid base element
 		if ( typeof base !== 'string' || base === '' ) {
@@ -36,6 +39,53 @@ tns.slider_infinite = function( $ ) {
 			return false;
 		}
 
+		return slider_setup();
+	}
+	/**
+	 * Bind slider left and right controls
+	 */
+	function slider_controls() {
+		this.base_element.children( '.slide-left' ).on( 'click',
+			{ slider: this },
+			function ( e ) {
+				var slider = e.data.slider;
+				if ( slider.index > 0 ) {
+					slider.index = slider.index - 1;
+				}
+				else if ( slider.index === 0 ) {
+					slider.index = slider.max_index;
+				}
+				update_index( slider.index );
+			} 
+		);
+		this.base_element.children( '.slide-right' ).on( 'click',
+			{ slider: this },
+			function ( e ) {
+				var slider = e.data.slider;
+				if ( slider.index < slider.max_index - 1 ) {
+					slider.index = slider.index + 1;
+					
+					if ( slider.index == slider.max_index - 1 ) {
+						var remainder = slider.element_count % slider.slides_visible;
+						debug.log( remainder );
+						if ( remainder > 0 ) {
+							update_index( ( slider.index - ( remainder / slider.slides_visible ) ) );
+							return;
+						}
+					}
+				}
+				else if ( slider.index == slider.max_index - 1 ) {
+					slider.index = 0;
+				}
+				update_index( slider.index );
+			} 
+		);
+	}
+	/**
+	 * Setup slider properties based on slider and individual slide widths
+	 * Registers a resize event to handle viewport size changes
+	 */
+	function slider_setup() {
 		// Setup slider element properties; uses the first slide to establish uniform slide width
 		this.index			= 0;
 		this.outer_width	= this.slider_body.width();
@@ -56,6 +106,7 @@ tns.slider_infinite = function( $ ) {
 		}
 		
 		this.max_index	= Math.ceil( this.element_count / this.slides_visible );
+		update_index( 0 );
 		
 		// Property debug detail
 		debug.log( 'Slider width: '			+ this.outer_width );
@@ -65,41 +116,26 @@ tns.slider_infinite = function( $ ) {
 		debug.log( 'Current slides: '		+ this.index );
 		debug.log( 'Max slides: '			+ this.max_index );
 		
+		// Reset parameters on resizes (delay of 500ms)
+		$( window ).resize( _.debounce( function() {
+			slider_setup();
+		}, 500 ) );
+		
 		return true;
 	}
-	function slider_controls() {
-		this.base_element.children( '.slide-left' ).on( 'click',
-			{ slider: this },
-			function ( e ) {
-				var slider = e.data.slider;
-				if ( slider.index > 0 ) {
-					slider.index = slider.index - 1;
-
-					var slide_position = slider.index * -100 + '%',
-						next = 'translate3d(' + slide_position + ', 0, 0 )';
-					slider.base_element.children( '.slider-body' ).css( 'transform', next );
-				}
-			} 
-		);
-		this.base_element.children( '.slide-right' ).on( 'click',
-			{ slider: this },
-			function ( e ) {
-				var slider = e.data.slider;
-				if ( slider.index < slider.max_index - 1 ) {
-					slider.index = slider.index + 1;
-
-					var slide_position = slider.index * -100 + '%',
-						next = 'translate3d(' + slide_position + ', 0, 0 )';
-					slider.base_element.children( '.slider-body' ).css( 'transform', next );
-				}
-			} 
-		);
-	}
+	/**
+	 * Update the slider position based on a supplied index
+	 * 
+	 * @param number index Number which is multiplied to a negative horizontal percentage position
+	 */
 	function update_index( index ) {
-		var slide_position = this.index * -100 + '%',
-			next = 'translate3d(' + slide_position + ', 0, 0 )';
-		debug.log( next );
-		this.base_element.css( 'transform', next );
+		var slide_position = 0;
+		if ( typeof index === 'number' && index > 0 ) { 
+			slide_position = index * -100 + '%';
+		}
+		var translation = 'translate3d(' + slide_position + ', 0, 0 )';
+		debug.log( translation );
+		this.slider_body.css( 'transform', translation );
 	}
 	return {
 		init: function( base ) {
@@ -114,4 +150,4 @@ tns.slider_infinite = function( $ ) {
 			}
 		}
 	}
-}( jQuery );
+}( jQuery, _ );
