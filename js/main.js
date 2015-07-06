522d862abb6f87243b79899868ff6dba43ce6912
rleeson <sysadmin@ryanleeson.com>
2015-07-06 01:54:04 -0400

    Add wrapping elements when the number of elements exceeds the number of visible elements and allows wrapping
    - Append/prepend cloned first/last elements to add the illusion of wrapping
    - Adjust the translation percentage to accommodate the additional slides

--- js/main.js
+++ js/main.js
@@ -9,11 +9,15 @@ var tns 	= tns || {};
 tns.slider_infinite = function( $, _ ) {
 	var base_element	= null,
 		base_selector	= '',
+		container		= null,
 		element_count	= false,
+		element_first	= null,
+		element_last	= null,
 		ie_version		= 0,
 		index			= 0,
 		max_index		= 0,
 		outer_width		= 0,
+		scrolling		= false,
 		slide_percent	= 0,
 		slide_width		= 0,
 		slider_elements	= null,
@@ -36,10 +40,12 @@ tns.slider_infinite = function( $, _ ) {
 		}
 
 		this.base_selector		= base;
+		this.container			= $( this.base_selector );
 		this.base_element		= $( this.base_selector + ' .slide-mask' );
 		this.slider_body		= this.base_element.find( '.slider-body' );
-		this.slider_elements	= this.slider_body.children( '.slide' ); 
+		this.slider_elements	= this.slider_body.children( '.slide' );
 		this.element_count		= this.slider_elements.length;
+		this.scrolling			= false;
 		
 		// Verify there are slides
 		if ( this.element_count < 1 ) {
@@ -47,6 +53,11 @@ tns.slider_infinite = function( $, _ ) {
 			return false;
 		}
 
+		this.element_first	= $( this.slider_elements.get( 0 ) ).clone().addClass( 'slide-temp' );
+		this.element_last	= $( this.slider_elements.get( this.element_count - 1 ) ).clone().addClass( 'slide-temp' );
+		debug.log( 'First: ' + this.element_first );
+		debug.log( 'Last: ' + this.element_last );
+		
 		return slider_setup();
 	}
 	// Check IE version number 
@@ -68,6 +79,11 @@ tns.slider_infinite = function( $, _ ) {
 				else if ( slider.index === 0 ) {
 					slider.index = slider.max_index - 1;
 				}
+				
+				// On first scroll event, setup scrolling
+				if ( slider.scrolling === false ) {
+					scroll_setup();
+				}
 				update_index( slider.index );
 			} 
 		);
@@ -81,9 +97,30 @@ tns.slider_infinite = function( $, _ ) {
 				else if ( slider.index == slider.max_index - 1 ) {
 					slider.index = 0;
 				}
+
+				// On first scroll event, setup scrolling
+				if ( slider.scrolling === false ) {
+					scroll_setup();
+				}
 				update_index( slider.index );
 			} 
 		);
+	}
+	function scroll_setup() {
+		// Latch scrolling and setup scroll actions
+		this.scrolling = true;
+		this.container.addClass( 'scrolling' );
+		
+		// Add edge wrapping elements if there are enough slides
+		if ( this.element_count > this.slides_visible + 1 ) {
+			if ( this.element_first ) {
+				this.slider_body.append( this.element_first );
+			}
+			if ( this.element_last ) {
+				this.slider_body.prepend( this.element_last );
+			}
+		}
+
 	}
 	/**
 	 * Setup slider properties based on slider and individual slide widths
@@ -99,9 +136,9 @@ tns.slider_infinite = function( $, _ ) {
 		var slides_shown	= this.outer_width / this.slide_width
 		this.slides_visible	= Math.ceil( slides_shown );
 		var width_variance	= this.slides_visible - slides_shown
-		debug.log( 'Slide width variance: ' + width_variance );
 
 		// Ensure tolerance with one-hundreth of a slide
+		debug.log( 'Slide width variance: ' + width_variance );
 		if ( width_variance < 0.99 && width_variance > 0.01 ) {
 			debug.log( 'Slide alignment is off, partial slide shown in slider.' );
 			return false;
@@ -122,10 +159,10 @@ tns.slider_infinite = function( $, _ ) {
 		debug.log( 'Current slides: '		+ this.index );
 		debug.log( 'Max slides: '			+ this.max_index );
 		
-		// Reset parameters on resizes (delay of 500ms)
+		// Reset parameters on resizes (delay of 1000ms)
 		$( window ).resize( _.debounce( function() {
 			slider_setup();
-		}, 500 ) );
+		}, 1000 ) );
 		
 		return true;
 	}
@@ -136,24 +173,39 @@ tns.slider_infinite = function( $, _ ) {
 	 */
 	function update_index( index ) {
 		var slide_position = 0;
+		
+		// Calculate non-zero index
 		if ( typeof index === 'number' && index > 0 ) {
+			// Handle last visible pane display
 			if ( index == this.max_index - 1 ) {
+				// Adjust partial index based on uneven visible slides / total slides ratio
 				var remainder = this.element_count % this.slides_visible;
 				debug.log( remainder );
 				if ( remainder > 0 ) {
 					index = index - ( remainder / this.slides_visible );
 				}
 			}
+			if ( this.element_last && ( this.element_count > this.slides_visible + 1 ) ) {
+				index = index + ( 1 / this.slides_visible );
+			}
 			slide_position = index * -100 + '%';
 		}
+		// Prepend the last element before the slider, if scrolling and it's not a duplicate of an visible slide
+		if ( index === 0 && this.container.hasClass( 'scrolling' ) ) {
+			if ( this.element_last && ( this.element_count > this.slides_visible + 1 ) ) {
+				slide_position = ( 1 / this.slides_visible ) * -100 + '%';
+			}
+		} 
+		
+		// Create 2D translation for IE9, 3D translation for all others
 		var translation		= 'translate3d( ' + slide_position + ', 0, 0 )',
 			ms_translation	= 'translate(' + slide_position + ', 0)';
-		debug.log( 'Standard: ' + translation );
-		debug.log( 'Microsoft: ' + ms_translation );
 		if ( this.ie_version && this.ie_version == 9 ) {
+			debug.log( 'Microsoft: ' + ms_translation );
 			this.slider_body.css( 'transform', ms_translation );
 		}
 		else {
+			debug.log( 'Standard: ' + translation );
 			this.slider_body.css( 'transform', translation );
 		}
 	}
